a40925b9bf6aa8583c1103f97d2cb08f32c145a8
Ramchandra Warang <ramchandra.warang@aissmsioit.org>
2023-03-04 16:44:16 +0530

    adding css and logic

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,23 +1,63 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const Chat = ({ socket, userName, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if ( currentMessage !== "" ) {
+      const messageData = {
+        room: room,
+        author: userName,
+        message: currentMessage,
+        time: (new Date(Date.now()).getHours()) + ":" + ((new Date(Date.now()).getMinutes())<10? "0": "" + new Date(Date.now()).getMinutes()),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+  
+
 
   return (
-    <div>
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => {
+          return (
+            <div className="message" id={userName === messageContent.author ? "you": "other"}>
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <div className="chat-footer">
         <input
           type="text"
           placeholder="Hey..."
           onChange={(e) => {
-            setRoom(e.target.value);
+            setCurrentMessage(e.target.value);
           }}
         />
-        <button>Send</button>
+        <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
